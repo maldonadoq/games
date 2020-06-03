ca76bdc24af148817a40a5ce4838fc855a22e7cd
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-06-03 17:37:20 -0500

    snake

--- lab3/data.py
+++ lab3/data.py
@@ -1,20 +1,82 @@
-from snake import Game
+from snake import *
 
 class SnakeData:
-	def __init__(self, game):
-		self.game = game
+	def __init__(self):		
 		self.train_x = []
 		self.train_y = []
 
 	def generate(self, games, steps):
 		for _ in range(games):
-			snake_start, snake_pos, apple_pos, score = self.game.starting_position()
-			apple_dist = self.game.apple_dist_from_snake(apple_pos, snake_pos)
+			snake_start, snake_pos, apple_pos, score = starting_position()
+			apple_dist = apple_dist_from_snake(apple_pos, snake_pos)
 
 			for _ in range(steps):
-				angle, snake_dir_vect, apple_dir_vect_norm, snake_dir_vect_norm = self.game.angle_with_apple(snake_pos, apple_pos)
+				angle, snake_dir_vect, apple_dir_vect_norm, snake_dir_vect_norm = angle_with_apple(snake_pos, apple_pos)
 
-				direction, button_direction = self.game.generate_random_direction(snake_pos, angle)
-				curr_dir_vect, front_blocked, left_blocked, right_blocked = self.game.blocked_directions(snake_pos)
+				direction, button_direction = generate_random_direction(snake_pos, angle)
+				curr_dir_vect, front_block, left_block, right_block = block_directions(snake_pos)
 
-				
+				direction, button_direction = self.generate_training_data_y(snake_pos, angle,
+																			button_direction, direction,
+																			front_block, left_block, right_block)					
+
+				if(front_block == 1 and left_block == 1 and right_block == 1):
+					break
+			
+				self.train_x.append([left_block, front_block, right_block, apple_dir_vect_norm[0]],
+									snake_dir_vect_norm[0], apple_dir_vect_norm[1], snake_dir_vect_norm[1])
+
+				snake_pos, apple_pos, score = play(snake_start, snake_pos, apple_pos,
+															button_direction, score)
+
+		return self.train_x, self.train_y
+
+	def generate_training_data_y(self, snake_pos, angle, button_direction, direction, front_block, left_block, right_block):
+		if(direction == -1):
+			if(left_block == 1):
+				if(front_block == 1 and right_block == 0):
+					direction, button_direction = direction_vector(snake_pos, angle, 1)
+					self.train_y.append([0,0,1])
+				elif(front_block == 0 and right_block == 1):
+					direction, button_direction = direction_vector(snake_pos, angle, 0)
+					self.train_y.append([0,1,0])
+				elif(front_block == 0 and right_block == 0):
+					direction, button_direction = direction_vector(snake_pos, angle, 1)
+					self.train_y.append([0,0,1])
+			else:
+				self.train_y.append([1,0,0])
+		
+		elif(direction == 0):
+			if(front_block == 1):
+				if(left_block == 1 and right_block == 0):
+					direction, button_direction = direction_vector(snake_pos, angle, 1)
+					self.train_y.append([0,0,1])
+				elif(left_block == 0 and right_block == 1):
+					direction, button_direction = direction_vector(snake_pos, angle, -1)
+					self.train_y.append([1,0,0])
+				elif(left_block == 0 and right_block == 0):
+					direction, button_direction = direction_vector(snake_pos, angle, 1)
+					self.train_y.append([0,0,1])
+			else:
+				self.train_y.append([0,1,0])
+		
+		else:
+			if(right_block == 1):
+				if(left_block == 1 and front_block == 0):
+					direction, button_direction = direction_vector(snake_pos, angle, 0)
+					self.train_y.append([0,1,0])
+				elif(left_block == 0 and front_block == 1):
+					direction, button_direction = direction_vector(snake_pos, angle, -1)
+					self.train_y.append([1,0,0])
+				elif(left_block == 0 and front_block == 0):
+					direction, button_direction = direction_vector(snake_pos, angle, -1)
+					self.train_y.append([1,0,0])
+			else:
+				self.train_y.append([0,0,1])
+
+		return direction, button_direction
+
+if __name__ == "__main__":
+	data = SnakeData()
+
+	data.generate(10, 20)
--- lab3/snake.py
+++ lab3/snake.py
@@ -1,15 +1,97 @@
-import pygame as pg
 import numpy as np
 import random
+import math
 
-class Game:
-	def starting_position(self):
-		snake_start = [100,100]
-		snake_pos = [[100,100], [90,100], [80,100]]
-		apple_pos = [random.randrange(1,50)*10, random.randrange(1,50)*10]
-		score = 3
+#class Game:
+def starting_position():
+	snake_start = [100,100]
+	snake_pos = [[100,100], [90,100], [80,100]]
+	apple_pos = [random.randrange(1,50)*10, random.randrange(1,50)*10]
+	score = 3
 
-		return snake_start, snake_pos, apple_pos, score
+	return snake_start, snake_pos, apple_pos, score
 
-	def apple_dist_from_snake(self, apple_pos, snake_pos):
-		return np.linalg.norm(np.array(apple_pos) - np.array(snake_pos[0]))
+def apple_dist_from_snake(apple_pos, snake_pos):
+	return np.linalg.norm(np.array(apple_pos) - np.array(snake_pos[0]))
+
+def angle_with_apple(snake_pos, apple_pos):
+	apple_dir_vect = np.array(apple_pos) - np.array(snake_pos[0])
+	snake_dir_vect = np.array(snake_pos[0]) - np.array(snake_pos[1])
+
+	norm_of_apple_dir_vect = np.linalg.norm(apple_dir_vect)
+	norm_of_snake_dir_vect = np.linalg.norm(snake_dir_vect)
+	if(norm_of_apple_dir_vect == 0):
+		norm_of_apple_dir_vect = 10
+	if(norm_of_snake_dir_vect == 0):
+		norm_of_snake_dir_vect = 10
+
+	apple_dir_vect_norm = apple_dir_vect / norm_of_apple_dir_vect
+	snake_dir_vect_norm = snake_dir_vect / norm_of_snake_dir_vect
+	angle = math.atan2(
+			apple_dir_vect_norm[1] * snake_dir_vect_norm[0] - apple_dir_vect_norm[0] * snake_dir_vect_norm[1],
+			apple_dir_vect_norm[1] * snake_dir_vect_norm[1] + apple_dir_vect_norm[0] * snake_dir_vect_norm[0]
+		) / math.pi
+	return angle, snake_dir_vect, apple_dir_vect_norm, snake_dir_vect_norm
+
+def generate_random_direction(snake_pos, angle_with_apple):
+	direction = 0
+	if(angle_with_apple > 0):
+		direction = 1
+	elif(angle_with_apple < 0):
+		direction = -1
+	else:
+		direction = 0
+
+	return direction_vector(snake_pos, angle_with_apple, direction)
+
+
+def direction_vector(snake_pos, angle_with_apple, direction):
+	curr_dir_vect = np.array(snake_pos[0]) - np.array(snake_pos[1])
+	left_dir_vect = np.array([curr_dir_vect[1], -curr_dir_vect[0]])
+	right_dir_vect = np.array([-curr_dir_vect[1], curr_dir_vect[0]])
+
+	new_direction = curr_dir_vect
+
+	if(direction == -1):
+		new_direction = left_dir_vect
+	if(direction == 1):
+		new_direction = right_dir_vect
+
+	button_direction = generate_button_direction(new_direction)
+
+	return direction, button_direction
+
+
+def generate_button_direction(new_direction):
+	button_direction = 0
+	if(new_direction.tolist() == [10, 0]):
+		button_direction = 1
+	elif(new_direction.tolist() == [-10, 0]):
+		button_direction = 0
+	elif(new_direction.tolist() == [0, 10]):
+		button_direction = 2
+	else:
+		button_direction = 3
+
+	return button_direction
+
+def block_directions(snake_pos):
+    curr_dir_vect = np.array(snake_pos[0]) - np.array(snake_pos[1])
+
+    left_dir_vect = np.array([curr_dir_vect[1], -curr_dir_vect[0]])
+    right_dir_vect = np.array([-curr_dir_vect[1], curr_dir_vect[0]])
+
+    front_block = direction_block(snake_pos, curr_dir_vect)
+    left_block = direction_block(snake_pos, left_dir_vect)
+    right_block = direction_block(snake_pos, right_dir_vect)
+
+    return curr_dir_vect, front_block, left_block, right_block
+
+
+def direction_block(snake_pos, curr_dir_vect):
+    next_step = snake_pos[0] + curr_dir_vect
+    snake_start = snake_pos[0]
+    if(collision_with_boundaries(next_step) == 1 or collision_with_self(next_step.tolist(), snake_pos) == 1):
+        return 1
+    else:
+        return 0
