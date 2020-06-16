7d21fe482a1e5d9f7187502d9f9d76e395eefbea
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-06-15 19:13:11 -0500

    ReLU Activation Function

---
+++ nn/layers/relu_activation.cuh
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "nn_layer.cuh"
+#include "../utils/nn_exception.cuh"
+
+class ReLUActivation : public NNLayer {
+private:
+	Matrix A;
+
+	Matrix Z;
+	Matrix dZ;
+
+public:
+	ReLUActivation(std::string name);
+	~ReLUActivation();
+
+	Matrix& forward(Matrix& Z);
+	Matrix& backprop(Matrix& dA, float learning_rate = 0.01);
+};
