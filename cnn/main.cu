c92a6dba5eb2f6fdbabd241af03c9469583d133c
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-06-29 15:42:09 -0500

    main file

---
+++ cnn/main.cu
@@ -0,0 +1,13 @@
+#include "neurals/dataset.cuh"
+#include "neurals/mnist.cuh"
+
+#define batch_size 256
+
+int main(int argc, char *argv[]){
+	int epochs = 3;
+
+	Mnist mnist("data", 0.003, 0.0001, 0.99);
+	
+	mnist.train(epochs, batch_size);
+	mnist.test(batch_size);
+}
